d776f9e16a9cae93722322ef80e359a13c72ae65
alanphil2k01 <alanphilipose@gmail.com>
2020-10-15 19:12:49 +0530

    changed setup script

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # My vim config
 
-### Setup
+## Setup
 
+### Manual Method
 ```
 git clone https://github.com/alanphil2k01/vim-config.git
 cd vim-config
-chmod +x setup.sh
-./setup.sh
+cp .vimrc ~/.vimrc
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall // This will install all the plugins
+vim ~/.vimrc // This will install coc-extensions
+```
+
+### Automatic method
+```
+curl -O https://raw.githubusercontent.com/alanphil2k01/vim-config/master/setup.sh 
+sh setup.sh
 ```
--- setup.sh
+++ setup.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
 
+# git clone repo
+clear
+echo "Cloning Repo"
+git clone https://github.com/alanphil2k01/vim-config.git
+cd vim-config
+
 # Copying config files
+clear
 echo "Copying vim files"
-cp -r .vim .vimrc $HOME
+cp -r .vimrc ~/.vimrc
+
+# Installing vim plug
+clear
+echo "Installing vim-plug"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Installing plugins 
+clear
 echo "Installing plugins with Plug"
-vim +PlugInstall&
-vim +CocInstall&
+echo "Quit out of after its done by :q"
+sleep 5
+vim +PlugInstall
+
+# Installing 
+clear
+echo "Opening .vimrc to install coc-extensions"
+vim ~/.vimrc
