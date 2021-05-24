83bf63cbcae2ed52a244500cf50eb5bbb96035d4
alanphil2k01 <alanphilipose@gmail.com>
2021-05-24 11:01:20 +0530

    fix in setup scripts

--- setup-nvim.sh
+++ setup-nvim.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # Check for dependencies
-clear
 echo "Checking for dependencies"
 if ! [ -x "$(command -v node)" ]; then
     echo 'Error: node is not installed.' >&2
@@ -13,46 +12,41 @@ if ! [ -x "$(command -v npm)" ]; then
 fi
 
 # git clone repo
-clear
 echo "Cloning Repo"
-git clone https://github.com/alanphil2k01/vim-config.git
+git clone https://github.com/alanphil2k01/vim-config.git > /dev/null
 cd vim-config/nvim
 
 # Creating required directories
 mkdir -p ~/.config/nvim
 
 # Copying config files
-clear
 echo "Copying vim files"
-cp -r init.vim ~/.config/nvim/init.vim
-cp -r plugin ~/.config/nvim/
+cp init.vim ~/.config/nvim/init.vim > /dev/null
+cp -r plugin ~/.config/nvim/ > /dev/null
 
 # Installing vim plug
-clear
 echo "Installing vim-plug"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' > /dev/null
 
 # Installing plugins 
-clear
 echo "Installing plugins with Plug"
+echo "Ignore Errors by pressing Enter"
 echo "Quit out of after its done by :qall"
 sleep 5
 nvim +PlugInstall
 
 # Installing coc extensions
-clear
 echo "Opening init.vim to install coc-extensions"
+echo "Ignore Errors by pressing Enter"
 echo "Once done close out of it with :qall"
 sleep 5
 nvim ~/.config/nvim/init.vim
 
 # Deleting the repository
-clear
 echo "Deleting vim-config repository"
 cd ../
-rm -rf vim-config
+rm -rf vim-config > /dev/null
 
 # Done
-clear
 echo "All Done..."
--- setup.sh
+++ setup.sh
@@ -13,42 +13,35 @@ if ! [ -x "$(command -v npm)" ]; then
 fi
 
 # git clone repo
-clear
 echo "Cloning Repo"
-git clone https://github.com/alanphil2k01/vim-config.git
+git clone https://github.com/alanphil2k01/vim-config.git > /dev/null
 cd vim-config
 
 # Copying config files
-clear
 echo "Copying vim files"
-cp -r .vimrc ~/.vimrc
+cp .vimrc ~/.vimrc > /dev/null
 
 # Installing vim plug
-clear
 echo "Installing vim-plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null
 
 # Installing plugins 
-clear
 echo "Installing plugins with Plug"
 echo "Quit out of after its done by :qall"
 sleep 5
 vim +PlugInstall
 
 # Installing 
-clear
 echo "Opening .vimrc to install coc-extensions"
 echo "Once done close out of it with :qall"
 sleep 5
 vim ~/.vimrc
 
 # Deleting the repository
-clear
 echo "Deleting vim-config repository"
 cd ../
-rm -rf vim-config
+rm -rf vim-config > /dev/null
 
 # Done
-clear
 echo "All Done..."
