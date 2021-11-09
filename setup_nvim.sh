5ff14f4aeaafd865ef6d6ad9c28a72a0c148a457
alanphil2k01 <alanphilipose@gmail.com>
2021-11-09 23:25:23 +0530

    added an install script

---
+++ setup_nvim.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+git clone https://github.com/alanphil2k01/vim-config.git
+cd vim-config
+mkdir -p ~/.config
+cp -r nvim ~/.config
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim +PlugInstall
